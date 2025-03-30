6f13139851086ce7cdc1119f8199334889e53bed
wontasy <wontasy@users.noreply.github.com>
2025-03-30 06:24:07 +0000

    deploy: e906deb365375a045990b320621fbd428fab619a

---
+++ config.js
@@ -0,0 +1,1 @@
+const config = { liffId_HighBrand: '2007158573-PZyndKqE', gasUrl: 'https://script.google.com/macros/s/AKfycbyPEH0WRKA3YMmlX-rUbEQrVHAnze6x7SLlaXav9Xuauj_AibJE8qk2CAyuWNc04I50cg/exec' };
